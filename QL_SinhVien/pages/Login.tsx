e833593ae3b0d2edd4002069cee041b0a6339d4c
chaucbblk6-design <chaucbblk6@gmail.com>
2026-01-22 19:48:44 +0700

    Fix server.js: Auto create admin account

--- QL_SinhVien/pages/Login.tsx
+++ QL_SinhVien/pages/Login.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { GraduationCap, Lock, User as UserIcon, AlertCircle } from 'lucide-react';
 
+
 const Login: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
